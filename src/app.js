2b7af6062500dcdbf3a043a26196020b4332cbeb
diemmee <138741915+diemmee@users.noreply.github.com>
2023-09-26 22:49:27 +0700

    Implement an error handling middleware

--- src/app.js
+++ src/app.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const handeError = require("./handle-error");
+const RiceDiseasesController = require("./controllers/RiceDiseasesController");
 
 app.use(cors());
 app.get("/", (req, res) => res.json({ message: "Hello World!" }));
+
+app.route("/api/rice-diseases").get(RiceDiseasesController.getAllRiceDiseases);
+
+app.use((req, res, next) => {
+    return next(new handeError(404, "Resource not found"));
+});
+app.use((error, req, res) => {
+    return res.status(error.statusCode || 500).json({
+        message: error.message || "Internal Server Error",
+    });
+});
 
 module.exports = app;
